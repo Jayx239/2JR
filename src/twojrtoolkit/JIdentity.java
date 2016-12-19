1766a225331327501130cbd429791da7172d33d1
Jason <Jason>
2016-12-19 17:50:52 -0500

    Added JIdentity class

---
+++ src/twojrtoolkit/JIdentity.java
@@ -0,0 +1,25 @@
+/**
+ * JIdentity class
+ * <p>
+ * 
+ */
+package twojrtoolkit;
+
+/**
+ * @author Jason Gallagher
+ *
+ */
+public class JIdentity {
+	
+	public JIdentity(){
+		
+	}
+	
+	public JIdentity(int inId, String inName){
+		id = inId;
+		name = inName;
+	}
+	
+	int id;
+	String name;
+}
