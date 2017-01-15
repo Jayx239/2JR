0af7b7dee77da11e2566b9052fd0a018a6c63f95
unknown <rcunni002c@patapl-1TYRZ52.cable.comcast.com>
2017-01-15 15:12:53 -0500

    Added JData Sizes Enum for 1 to 16 bytes

--- src/com/twojr/toolkit/JDataSizes.java
+++ src/com/twojr/toolkit/JDataSizes.java
@@ -3,5 +3,25 @@ package com.twojr.toolkit;
 /**
  * Created by rcunni002c on 1/15/2017.
  */
-public class JDataSizes {
-}
+public enum JDataSizes {
+
+    EIGHT_BIT,                  //8 bit, 1 byte
+    SIXTEEN_BIT,                //16 bit, 2 byte
+    TWENTY_FOUR_BIT,            //24 bit, 3 byte
+    THIRTY_TWO_BIT,             //32 bit, 4 byte
+    FORTY_BIT,                  //40 bit, 5 byte
+    FORTY_EIGHT_BIT,            //48 bit, 6 byte
+    FIFTY_SIX_BIT,              //56 bit, 7 byte
+    SIXTY_FOUR_BIT,             //64 bit, 8 byte
+    SEVENTY_TWO_BIT,            //72 bit, 9 byte
+    EIGHTY_BIT,                 //80 bit, 10 byte
+    EIGHTY_EIGHT_BIT,           //88 bit, 11 byte
+    NINETY_SIX_BIT,             //96 bit, 12 byte
+    HUNDRED_FOUR_BIT,           //104 bit, 13 byte
+    HUNDRED_TWELVE_BIT,         //112 bit, 14 byte
+    HUNDRED_TWENTY_BIT,         //120 bit, 15 byte
+    HUNDRED_TWENTY_EIGHT_BIT    //128 bit, 16 byte
+
+} /*********************************************END OF FILE*************************************************************/
+
+
