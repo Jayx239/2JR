63c9557c7806b840f1933c84a207a562891cf6a0
Jayx239 <grazinpalm@gmail.com>
2017-01-17 15:00:37 -0500

    JBitTest initial commit, toByte is failing tests.

---
+++ src/com/twojr/toolkit/test/JBitTest.java
@@ -0,0 +1,110 @@
+package com.twojr.toolkit.test;
+import static org.junit.Assert.*;
+
+import com.twojr.toolkit.JBit;
+import com.twojr.toolkit.JDouble;
+import org.junit.Test;
+import java.nio.ByteBuffer;
+/**
+ * Created by Jason on 1/17/2017.
+ */
+public class JBitTest {
+    @Test
+    public void evaluateToByte1() {
+        boolean value = true;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit1 = new JBit(id,name,size,value);
+
+        assertEquals(0x01, bit1.toByte());
+    }
+
+    @Test
+    public void evaluateToByte0() {
+
+        boolean value = false;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit0 = new JBit(id,name,size,value);
+
+        assertEquals(0x00, bit0.toByte());
+    }
+
+    @Test
+    public void evaluateGetSize() {
+
+        boolean value = false;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit = new JBit(id,name,size,value);
+        assertEquals(size, bit.getSize());
+    }
+
+    @Test
+    public void evaluateGetValue() {
+
+        boolean value = false;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit = new JBit(id,name,size,value);
+
+        assertEquals(value,bit.getValue());
+    }
+
+    @Test
+    public void evalueateGetId() {
+
+        boolean value = false;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit = new JBit(id,name,size,value);
+        assertEquals(id, bit.getId());
+    }
+
+    @Test
+    public void evaluateGetName() {
+
+        boolean value = false;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit = new JBit(id,name,size,value);
+        assertEquals(name, bit.getName());
+    }
+
+    @Test
+    public void evaluatePrint1() {
+
+        boolean value = true;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit1 = new JBit(id,name,size,value);
+        assertEquals("1", bit1.print());
+    }
+
+    @Test
+    public void evaluatePrint0() {
+
+        boolean value = false;
+        int size = 1;
+        int id = 1;
+        String name = "name";
+
+        JBit bit1 = new JBit(id,name,size,value);
+        assertEquals("0", bit1.print());
+    }
+
+}
